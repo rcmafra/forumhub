88fdef69d57d90030b3f60723cb3820162832978
RaulMafra <raulcesar.sm@gmail.com>
2024-07-07 17:01:43 -0300

    refactor: refatoração no DTO de criação de tópico

--- src/main/java/com/backend/api/forumhub/dto/request/CreateTopicDTO.java
+++ src/main/java/com/backend/api/forumhub/dto/request/CreateTopicDTO.java
@@ -1,4 +1,4 @@
 package com.backend.api.forumhub.dto.request;
 
-public record CreateTopicDTO(String title, String message, Long user_id, Long course_id) {
+public record CreateTopicDTO(String title, String message, Long course_id) {
 }
