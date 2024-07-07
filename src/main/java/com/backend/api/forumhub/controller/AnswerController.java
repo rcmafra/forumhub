b203832e6d73f9d8b2320f3660e40d46b7f9145f
RaulMafra <raulcesar.sm@gmail.com>
2024-07-07 16:58:02 -0300

    feature: adicionado funcionalidade para obter o user_id através do token

--- src/main/java/com/backend/api/forumhub/controller/AnswerController.java
+++ src/main/java/com/backend/api/forumhub/controller/AnswerController.java
@@ -6,6 +6,8 @@ import com.backend.api.forumhub.service.AnswerService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -19,20 +21,29 @@ public class AnswerController {
     }
 
     @PostMapping("/{topic_id}/answer/")
-    private ResponseEntity<HttpMessage> answerTopic(@PathVariable Long topic_id, @Valid @RequestBody AnswerTopicDTO answerTopicDTO) throws Exception {
-        answerService.answerTopic(topic_id, answerTopicDTO);
+    private ResponseEntity<HttpMessage> answerTopic(@PathVariable Long topic_id, @Valid @RequestBody AnswerTopicDTO answerTopicDTO,
+                                                    @AuthenticationPrincipal Jwt jwt) throws Exception {
+
+        Long user_id = Long.parseLong(jwt.getClaim("user_id"));
+        answerService.answerTopic(topic_id, user_id, answerTopicDTO);
         return new ResponseEntity<>(new HttpMessage("HttpStatusCode OK"), HttpStatus.OK);
     }
 
     @PostMapping("/{topic_id}/answers/{answer_id}/")
-    private ResponseEntity<HttpMessage> markBetterAnswer(@PathVariable Long topic_id, @PathVariable Long answer_id) throws Exception {
-        this.answerService.markBetterAnswer(topic_id, answer_id);
+    private ResponseEntity<HttpMessage> markBetterAnswer(@PathVariable Long topic_id, @PathVariable Long answer_id,
+                                                         @AuthenticationPrincipal Jwt jwt) throws Exception {
+
+        Long user_id = Long.parseLong(jwt.getClaim("user_id"));
+        this.answerService.markBetterAnswer(topic_id, answer_id, user_id);
         return new ResponseEntity<>(new HttpMessage("HttpStatusCode OK"), HttpStatus.OK);
     }
 
     @DeleteMapping("/{topic_id}/answers/{answer_id}/")
-    private ResponseEntity<HttpMessage> deleteAnswer(@PathVariable Long topic_id, @PathVariable Long answer_id) throws Exception {
-        this.answerService.deleteAnswer(topic_id, answer_id);
+    private ResponseEntity<HttpMessage> deleteAnswer(@PathVariable Long topic_id, @PathVariable Long answer_id,
+                                                     @AuthenticationPrincipal Jwt jwt) throws Exception {
+
+        Long user_id = Long.parseLong(jwt.getClaim("user_id"));
+        this.answerService.deleteAnswer(topic_id, answer_id, user_id);
         return new ResponseEntity<>(new HttpMessage("HttpStatusCode OK"), HttpStatus.OK);
     }
 
