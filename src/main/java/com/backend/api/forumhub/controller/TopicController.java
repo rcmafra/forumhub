a470aeb133adcb74e7e05e343c4abee7c717d097
RaulMafra <raulcesar.sm@gmail.com>
2024-07-07 17:01:10 -0300

    feature: adicionado funcionalidade para obter o user_id através do token, além de restrição para atualização e deleção de um tópico

--- src/main/java/com/backend/api/forumhub/controller/TopicController.java
+++ src/main/java/com/backend/api/forumhub/controller/TopicController.java
@@ -14,6 +14,9 @@ import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.PagedModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -27,31 +30,44 @@ public class TopicController {
     }
 
     @PostMapping("/create/")
-    private ResponseEntity<HttpMessage> createTopic(@Valid @RequestBody CreateTopicDTO createTopicDTO) throws Exception {
-        this.topicService.createTopic(createTopicDTO);
+    private ResponseEntity<HttpMessage> createTopic(@Valid @RequestBody CreateTopicDTO createTopicDTO,
+                                                    @AuthenticationPrincipal Jwt jwt) throws Exception {
+
+        Assert.notNull(jwt,"Bearer token can't be empty");
+        Long user_id = Long.parseLong(jwt.getClaim("user_id"));
+        this.topicService.createTopic(createTopicDTO, user_id);
         return new ResponseEntity<>(new HttpMessage("HttpStatusCode OK"), HttpStatus.CREATED);
     }
 
     @GetMapping("/")
-    private PagedModel<EntityModel<GetTopicDTO>> topicsList(@PageableDefault() Pageable pageable, PagedResourcesAssembler<GetTopicDTO> assembler){
+    private PagedModel<EntityModel<GetTopicDTO>> topicsList(@PageableDefault Pageable pageable,
+                                                            PagedResourcesAssembler<GetTopicDTO> assembler) {
+
         Page<GetTopicDTO> topicsListDTOS = topicService.topicList(pageable);
         return assembler.toModel(topicsListDTOS);
     }
 
     @GetMapping("/{topic_id}/")
     private ResponseEntity<GetTopicDTO> getTopic(@PathVariable Long topic_id) throws Exception {
-        return ResponseEntity.ok(new GetTopicDTO(topicService.getTopic(topic_id)));
+
+        return ResponseEntity.ok(new
+                GetTopicDTO(topicService.getTopicById(topic_id)));
     }
 
 
     @PutMapping("/{topic_id}/")
-    private ResponseEntity<HttpMessage> updateTopic(@PathVariable Long topic_id,  @Valid @RequestBody UpdateTopicDTO updateTopicDTO) throws Exception {
-        this.topicService.updateTopic(topic_id, updateTopicDTO);
+    private ResponseEntity<HttpMessage> updateTopic(@PathVariable Long topic_id,  @Valid @RequestBody UpdateTopicDTO updateTopicDTO,
+                                                    @AuthenticationPrincipal Jwt jwt) throws Exception {
+
+        Long user_id = Long.parseLong(jwt.getClaim("user_id"));
+        this.topicService.updateTopic(topic_id, user_id, updateTopicDTO);
         return new ResponseEntity<>(new HttpMessage("HttpStatusCode OK"), HttpStatus.OK);
     }
 
     @DeleteMapping("/delete/{topic_id}/")
-    private ResponseEntity<HttpMessage> deleteTopic(@PathVariable Long topic_id, @RequestBody Long user_id) throws Exception {
+    private ResponseEntity<HttpMessage> deleteTopic(@PathVariable Long topic_id, @AuthenticationPrincipal Jwt jwt) throws Exception {
+
+        Long user_id = Long.parseLong(jwt.getClaim("user_id"));
         topicService.deleteTopic(topic_id, user_id);
         return new ResponseEntity<>(new HttpMessage("HttpStatusCode OK"), HttpStatus.OK);
     }
--- src/main/java/com/backend/api/forumhub/service/TopicService.java
+++ src/main/java/com/backend/api/forumhub/service/TopicService.java
@@ -8,6 +8,7 @@ import com.backend.api.forumhub.dto.request.CreateTopicDTO;
 import com.backend.api.forumhub.dto.request.UpdateTopicDTO;
 import com.backend.api.forumhub.dto.response.GetTopicDTO;
 import com.backend.api.forumhub.repository.TopicRepository;
+import com.backend.api.forumhub.validator.AuthorizationValidate;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -27,8 +28,8 @@ public class TopicService {
         this.courseService = courseService;
     }
 
-    public void createTopic(CreateTopicDTO createTopicDTO) throws Exception {
-        User author = userService.getUserById(createTopicDTO.user_id());
+    public void createTopic(CreateTopicDTO createTopicDTO, Long user_id) throws Exception {
+        User author = userService.getUserById(user_id);
         Course course = courseService.getCourseById(createTopicDTO.course_id());
 
         Topic topic = new Topic(createTopicDTO.title(), createTopicDTO.message(), author, course);
@@ -39,21 +40,19 @@ public class TopicService {
         return this.topicRepository.findAll(pageable).map(GetTopicDTO::new);
     }
 
-    public Topic getTopic(Long topic_id) throws Exception {
+
+    public Topic getTopicById(Long topic_id) throws Exception {
         return topicRepository.findById(topic_id).orElseThrow(() -> new Exception("Topic not found"));
     }
 
-    /*todo
-     *Obter o usuário logado atualmente e verificar se ele é o dono do tópico antes de
-     * efetuar o update desse tópico
-     * */
-    public void updateTopic(Long topic_id, UpdateTopicDTO update) throws Exception {
-        Topic topic = this.getTopic(topic_id);
-        Course course = this.courseService.getCourseById(update.course_id());
 
-        if (!topic.getAuthor().getId().equals(update.author_id())) {
-            throw new Exception("Topic not belonging to this author");
-        }
+    public void updateTopic(Long topic_id, Long user_id, UpdateTopicDTO update) throws Exception {
+        Topic topic = this.getTopicById(topic_id);
+        Course course = this.courseService.getCourseById(update.course_id());
+        User user = this.userService.getUserById(user_id);
+
+        AuthorizationValidate.AuthValidator(topic.getAuthor().getId(), user.getId(),
+                user.getProfile().getProfileName());
 
         topic.setTitle(update.topic().getTitle());
         topic.setMessage(update.topic().getMessage());
@@ -64,17 +63,13 @@ public class TopicService {
 
     }
 
-    /*todo
-    * Obter o usuário logado que enviou a request e verificar se foi ele quem criou o tópico
-    * */
+
     public void deleteTopic(Long topic_id, Long user_id) throws Exception {
-        Topic topic = this.getTopic(topic_id);
+        Topic topic = this.getTopicById(topic_id);
         User user = this.userService.getUserById(user_id);
 
-        if(!topic.getAuthor().getId().equals(user_id) || !(user.getProfile().getProfileName().equals(Profile.ProfileName.MOD) ||
-                user.getProfile().getProfileName().equals(Profile.ProfileName.ADM))){
-            throw new Exception("Insufficient privilege");
-        }
+        AuthorizationValidate.AuthValidator(topic.getAuthor().getId(), user.getId(),
+                user.getProfile().getProfileName());
 
         this.topicRepository.delete(topic);
     }
