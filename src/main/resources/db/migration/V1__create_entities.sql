e8260dd4cf0f1c58e9d7cbf949c5542e1413c762
RaulMafra <raulcesar.sm@gmail.com>
2024-07-07 17:09:10 -0300

    Ajustes no script SQL do flyway

--- src/main/resources/db/migration/V1__create_entities.sql
+++ src/main/resources/db/migration/V1__create_entities.sql
@@ -11,13 +11,13 @@ CREATE TABLE hub.users (
     password VARCHAR2(255) NOT NULL,
     profile_id NUMBER(19),
     PRIMARY KEY (user_id),
-    FOREIGN KEY (profile_id) REFERENCES hub.profile(profile_id)
+    CONSTRAINT FK_PROFILE_USERS FOREIGN KEY (profile_id) REFERENCES hub.profile(profile_id)
 );
 
 CREATE TABLE hub.course (
     course_id NUMBER(19) GENERATED AS IDENTITY,
-    name VARCHAR2(255),
-    category VARCHAR2(50 CHAR) NOT NULL UNIQUE CHECK (category IN ('JAVA','C','CPLUSPLUS','CSHARP','GOLANG','COMPUTATION','QA')),
+    name VARCHAR2(255) NOT NULL UNIQUE,
+    category VARCHAR2(50 CHAR) NOT NULL CHECK (category IN ('JAVA','C','CPLUSPLUS','CSHARP','GOLANG','COMPUTATION','QA')),
     PRIMARY KEY (course_id)
 );
 
@@ -25,24 +25,25 @@ CREATE TABLE hub.topic (
     topic_id NUMBER(19) GENERATED AS IDENTITY,
     title VARCHAR2(200) NOT NULL,
     message VARCHAR2(255) NOT NULL,
+    status VARCHAR2(10 CHAR) DEFAULT 'UNSOLVED' CHECK (status IN ('SOLVED', 'UNSOLVED')),
     created_at TIMESTAMP(6) WITH LOCAL TIME ZONE,
-    status VARCHAR2(10 CHAR) NOT NULL UNIQUE CHECK (status IN ('SOLVED', 'UNSOLVED')),
     user_id NUMBER(19),
     course_id NUMBER(19),
     PRIMARY KEY (topic_id),
-    FOREIGN KEY (user_id) REFERENCES hub.users(user_id),
-    FOREIGN KEY (course_id) REFERENCES hub.course(course_id)
+    CONSTRAINT FK_USERS_TOPIC FOREIGN KEY (user_id) REFERENCES hub.users(user_id),
+    CONSTRAINT FK_COURSE_TOPIC FOREIGN KEY (course_id) REFERENCES hub.course(course_id)
 );
 
 CREATE TABLE hub.answer (
     answer_id NUMBER(19) GENERATED AS IDENTITY,
-    better_answer NUMBER(1,0) NOT NULL CHECK (better_answer in (0,1)),
+    solution VARCHAR2(255) NOT NULL,
+    better_answer NUMBER(1,0) DEFAULT 0 CHECK (better_answer in (0,1)),
+    created_at TIMESTAMP(6) WITH LOCAL TIME ZONE,
     topic_id NUMBER(19),
     user_id NUMBER(19),
-    solution VARCHAR2(255) NOT NULL,
-    created_at TIMESTAMP(6) WITH LOCAL TIME ZONE,
     PRIMARY KEY (answer_id),
-    FOREIGN KEY (user_id) REFERENCES hub.users(user_id),
-    FOREIGN KEY (topic_id) REFERENCES hub.topic(topic_id),
-    FOREIGN KEY (answer_id) REFERENCES hub.topic(topic_id)
+    CONSTRAINT FK_USERS_ANSWER FOREIGN KEY (user_id) REFERENCES hub.users(user_id),
+    CONSTRAINT FK_TOPIC_ANSWER FOREIGN KEY (topic_id) REFERENCES hub.topic(topic_id)
 );
+
+CREATE SEQUENCE hub.users_seq START WITH 1 INCREMENT BY 50;
