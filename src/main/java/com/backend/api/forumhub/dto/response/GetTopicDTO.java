9002cf86ef7bf1d1f44710049af2c30497c0126a
RaulMafra <raulcesar.sm@gmail.com>
2024-07-07 17:02:20 -0300

    refactor: alterado o tipo de coleção das respostas para Set

--- src/main/java/com/backend/api/forumhub/dto/response/GetTopicDTO.java
+++ src/main/java/com/backend/api/forumhub/dto/response/GetTopicDTO.java
@@ -1,17 +1,13 @@
 package com.backend.api.forumhub.dto.response;
 
-import com.backend.api.forumhub.domain.Answer;
-import com.backend.api.forumhub.domain.Course;
-import com.backend.api.forumhub.domain.Topic;
-import com.backend.api.forumhub.domain.User;
-import com.backend.api.forumhub.domain.Status;
+import com.backend.api.forumhub.domain.*;
 
 import java.time.LocalDateTime;
-import java.util.List;
+import java.util.Set;
 
-public record GetTopicDTO(Long id, String title, String message, LocalDateTime createdAt, Status status, User author, Course course, List<Answer> answer) {
+public record GetTopicDTO(Long id, String title, String message, LocalDateTime createdAt, Status status, User author, Course course, Set<Answer> answer) {
 
     public GetTopicDTO(Topic topic){
-        this(topic.getId(), topic.getTitle(), topic.getMessage(), topic.getCreatedAt(), topic.getStatus(), topic.getAuthor(), topic.getCourse(), topic.getAnswer());
+        this(topic.getId(), topic.getTitle(), topic.getMessage(), topic.getCreatedAt(), topic.getStatus(), topic.getAuthor(), topic.getCourse(), topic.getAnswers());
     }
 }
