04441d8d8453865a1f7927834fb2f784111ead92
RaulMafra <raulcesar.sm@gmail.com>
2024-07-08 21:55:49 -0300

    refactor: remoção de método não utilizado

--- src/main/java/com/backend/api/forumhub/repository/CourseRepository.java
+++ src/main/java/com/backend/api/forumhub/repository/CourseRepository.java
@@ -4,10 +4,7 @@ import com.backend.api.forumhub.domain.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface CourseRepository extends JpaRepository<Course, Long> {
 
-    Optional<Course> findByName(String name);
 }
