c603a4f71ed0cd883ef3e43fae98a742127f7d2a
RaulMafra <raulcesar.sm@gmail.com>
2024-07-24 13:40:56 -0300

    feature: Ajustado o mapeamento das requisições para usuários específicos

--- src/main/java/com/backend/api/forumhub/repository/AnswerRepository.java
+++ src/main/java/com/backend/api/forumhub/repository/AnswerRepository.java
@@ -1,9 +1,15 @@
 package com.backend.api.forumhub.repository;
 
 import com.backend.api.forumhub.domain.Answer;
+import com.backend.api.forumhub.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface AnswerRepository extends JpaRepository<Answer, Long> {
+
+    Optional<List<Answer>> getAnswerByAuthor(User user_id);
 }
