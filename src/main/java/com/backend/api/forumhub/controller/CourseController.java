d44736d11532103bd1517af24430a38962915a3a
RaulMafra <raulcesar.sm@gmail.com>
2024-07-25 13:22:59 -0300

    feature: Ajustado a solicitação dos mapeamentos para usuários específicos

--- src/main/java/com/backend/api/forumhub/controller/CourseController.java
+++ src/main/java/com/backend/api/forumhub/controller/CourseController.java
@@ -9,6 +9,7 @@ import com.backend.api.forumhub.service.CourseService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -23,29 +24,36 @@ public class CourseController {
         this.courseService = courseService;
     }
 
-    @PostMapping("/create/")
-    private ResponseEntity<HttpMessage> createCourse(@Valid @RequestBody CreateCourseDTO createCourseDTO){
+    @PreAuthorize("hasRole('ADM') and hasAuthority('SCOPE_course:create')")
+    @PostMapping("/create")
+    public ResponseEntity<HttpMessage> createCourse(@Valid @RequestBody CreateCourseDTO createCourseDTO){
 
         this.courseService.createCourse(createCourseDTO);
         return new ResponseEntity<>(new HttpMessage("HttpStatusCode OK"), HttpStatus.CREATED);
     }
 
-    @GetMapping("/")
-    private ResponseEntity<List<CourseCollection>> getAllCourse(){
+    @PreAuthorize("authenticated")
+    @GetMapping
+    public ResponseEntity<List<CourseCollection>> getAllCourse(){
         List<CourseCollection> courseCollection = this.courseService.getAllCourse();
+
         return new ResponseEntity<>(courseCollection, HttpStatus.OK);
     }
 
+    @PreAuthorize("hasRole('ADM') and hasAuthority('SCOPE_course:delete')")
     @DeleteMapping("/delete/{courseName}")
-    private ResponseEntity<HttpMessage> deleteCourse(@PathVariable String courseName){
+    public ResponseEntity<HttpMessage> deleteCourse(@PathVariable String courseName){
         this.courseService.deleteCourse(courseName);
+
         return ResponseEntity.ok(new HttpMessage("HttpStatusCode OK"));
 
     }
 
+    @PreAuthorize("hasRole('ADM') and hasAuthority('SCOPE_course:edit')")
     @PutMapping("/{courseName}")
-    private ResponseEntity<CourseDTO> updateNameCourse(@PathVariable String courseName, @RequestBody UpdateCourse updateCourse){
+    public  ResponseEntity<CourseDTO> updateNameCourse(@PathVariable String courseName, @RequestBody UpdateCourse updateCourse){
         CourseDTO courseDTO = this.courseService.updateNameCourse(courseName, updateCourse);
+
         return ResponseEntity.ok(courseDTO);
     }
 
