40213d24cf56f192fd9d76e1c918c44a8e949647
RaulMafra <raulcesar.sm@gmail.com>
2024-07-27 13:44:24 -0300

    refactor: Removida a anotação não utilizada

--- src/main/java/com/backend/api/forumhub/repository/TopicRepository.java
+++ src/main/java/com/backend/api/forumhub/repository/TopicRepository.java
@@ -2,7 +2,6 @@ package com.backend.api.forumhub.repository;
 
 import com.backend.api.forumhub.domain.Topic;
 import com.backend.api.forumhub.domain.User;
-import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,7 +9,6 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-@Transactional
 public interface TopicRepository extends JpaRepository<Topic, Long> {
 
      Optional<List<Topic>> getTopicByAuthor(User user_id);
