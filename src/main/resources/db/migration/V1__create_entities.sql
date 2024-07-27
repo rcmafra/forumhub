8ea0ff4fa4433c7f52b67a6d3faae3b2db554da6
RaulMafra <raulcesar.sm@gmail.com>
2024-07-27 13:46:33 -0300

    feature: Ajustado incremento do ID dos usuários

--- src/main/resources/db/migration/V1__create_entities.sql
+++ src/main/resources/db/migration/V1__create_entities.sql
@@ -5,7 +5,7 @@ CREATE TABLE hub.profile (
 );
 
 CREATE TABLE hub.users (
-    user_id NUMBER(19) NOT NULL,
+    user_id NUMBER(19) DEFAULT users_seq.nextval,
     name VARCHAR2(255) NOT NULL,
     email VARCHAR2(255) UNIQUE NOT NULL,
     password VARCHAR2(255) NOT NULL,
