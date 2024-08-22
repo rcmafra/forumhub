59008b5f261a6686beb9aac855ba850126619726
RaulMafra <raulcesar.sm@gmail.com>
2024-08-21 22:00:05 -0300

    Remoção de imports não utilizados

--- authorization-server/src/main/java/com/raul/forumhub/auth/server/security/config/JWKGenerate.java
+++ authorization-server/src/main/java/com/raul/forumhub/auth/server/security/config/JWKGenerate.java
@@ -6,7 +6,6 @@ import com.nimbusds.jose.jwk.source.JWKSource;
 import com.nimbusds.jose.proc.SecurityContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
 
--- topic/src/main/java/com/raul/forumhub/topic/TopicApplication.java
+++ topic/src/main/java/com/raul/forumhub/topic/TopicApplication.java
@@ -3,9 +3,6 @@ package com.raul.forumhub.topic;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @SpringBootApplication
 public class TopicApplication {
 
