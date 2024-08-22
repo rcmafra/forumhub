51b7882e5bd46818c1ce4db33cf135fc946132e0
RaulMafra <raulcesar.sm@gmail.com>
2024-08-21 21:54:32 -0300

    Correção de exceção na classe de validação de autorização

--- topic/src/main/java/com/raul/forumhub/topic/validator/AuthorizationValidate.java
+++ topic/src/main/java/com/raul/forumhub/topic/validator/AuthorizationValidate.java
@@ -1,6 +1,7 @@
 package com.raul.forumhub.topic.validator;
 
 import com.raul.forumhub.topic.domain.Profile;
+import com.raul.forumhub.topic.exception.AbstractServiceException;
 import lombok.experimental.UtilityClass;
 
 @UtilityClass
@@ -8,7 +9,7 @@ public class AuthorizationValidate {
 
     public static void AuthValidator(Long author_id, Long user_id, Profile.ProfileName role){
         if(!user_id.equals(author_id) && !(role.equals(Profile.ProfileName.MOD) || role.equals(Profile.ProfileName.ADM))){
-            throw new RuntimeException("Insufficient privilege");
+            throw new AbstractServiceException("Privilégio insuficiente");
         }
     }
 
