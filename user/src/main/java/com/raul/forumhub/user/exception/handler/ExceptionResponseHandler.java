725dafd1ca2367c7df76012b85637cca3d2521d2
RaulMafra <raulcesar.sm@gmail.com>
2024-08-21 21:59:03 -0300

    Criação de handler para lidar com as respostas de exceções

--- user/src/main/java/com/raul/forumhub/user/exception/handler/ExceptionResponseHandler.java
+++ user/src/main/java/com/raul/forumhub/user/exception/handler/ExceptionResponseHandler.java
@@ -1,9 +1,6 @@
-package com.raul.forumhub.topic.exception.handler;
+package com.raul.forumhub.user.exception.handler;
 
-import com.raul.forumhub.topic.exception.AbstractServiceException;
-import com.raul.forumhub.topic.exception.AnswerServiceException;
-import com.raul.forumhub.topic.exception.InstanceNotFoundException;
-import com.raul.forumhub.topic.exception.TopicServiceException;
+import com.raul.forumhub.user.exception.InstanceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.ConstraintViolation;
 import jakarta.validation.ConstraintViolationException;
@@ -15,11 +12,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.reactive.function.client.WebClientResponseException;
 
 import java.time.LocalDateTime;
 import java.util.Locale;
-import java.util.concurrent.TimeoutException;
 
 @RestControllerAdvice
 public class ExceptionResponseHandler {
@@ -54,26 +49,6 @@ public class ExceptionResponseHandler {
         return new ResponseEntity<>(entity, headers(), HttpStatus.BAD_REQUEST);
     }
 
-    @ExceptionHandler(WebClientResponseException.class)
-    public ResponseEntity<ExceptionEntity> webClientExceptionResolver(WebClientResponseException ex, HttpServletRequest request){
-        ExceptionEntity entity = new ExceptionEntity(LocalDateTime.now(), HttpStatus.SERVICE_UNAVAILABLE.value(),
-                "Serviço indisponível", "O serviço solicitado está fora do ar", request.getRequestURI());
-        return new ResponseEntity<>(entity, headers(), HttpStatus.SERVICE_UNAVAILABLE);
-    }
-
-    @ExceptionHandler(TimeoutException.class)
-    public ResponseEntity<ExceptionEntity> clientTimeoutExceptionResolver(TimeoutException ex, HttpServletRequest request){
-        ExceptionEntity entity = new ExceptionEntity(LocalDateTime.now(), HttpStatus.SERVICE_UNAVAILABLE.value(),
-                "Serviço indisponível", "O serviço solicitado está fora do ar", request.getRequestURI());
-        return new ResponseEntity<>(entity, headers(), HttpStatus.SERVICE_UNAVAILABLE);
-    }
-
-    @ExceptionHandler({AbstractServiceException.class, TopicServiceException.class, AnswerServiceException.class})
-    public ResponseEntity<ExceptionEntity> businessServiceExceptionResolver(AbstractServiceException ex, HttpServletRequest request){
-        ExceptionEntity entity = new ExceptionEntity(LocalDateTime.now(), HttpStatus.BAD_REQUEST.value(),
-                "Erro de negócio", ex.getMessage(), request.getRequestURI());
-        return new ResponseEntity<>(entity, headers(), HttpStatus.BAD_REQUEST);
-    }
 
     @ExceptionHandler(InstanceNotFoundException.class)
     public ResponseEntity<ExceptionEntity> instanceNotFoundExceptionResolver(InstanceNotFoundException ex, HttpServletRequest request){
