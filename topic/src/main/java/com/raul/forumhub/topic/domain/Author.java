5dec4851bebc87f24a9b432c652c6634fd0106ce
RaulMafra <raulcesar.sm@gmail.com>
2024-08-27 21:25:14 -0300

    Empty commit

--- topic/src/main/java/com/raul/forumhub/topic/domain/Author.java
+++ topic/src/main/java/com/raul/forumhub/topic/domain/Author.java
@@ -1,6 +1,5 @@
 package com.raul.forumhub.topic.domain;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
