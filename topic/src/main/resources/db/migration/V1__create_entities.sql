5fb605645dd5d116e6b1e88055c612134f30cdee
RaulMafra <raulcesar.sm@gmail.com>
2024-11-11 23:36:01 -0300

    Ajustes no versionamento das entidades no flyway

--- topic/src/main/resources/db/migration/V1__create_entities.sql
+++ topic/src/main/resources/db/migration/V1__create_entities.sql
@@ -1,3 +1,25 @@
+CREATE TABLE hub.profile (
+    profile_id NUMBER(19) GENERATED AS IDENTITY,
+    profile VARCHAR2(50 CHAR) NOT NULL UNIQUE CHECK (profile IN ('ADM','MOD','BASIC')),
+    PRIMARY KEY (profile_id)
+);
+
+CREATE TABLE hub.users (
+    user_id NUMBER(19) DEFAULT users_seq.nextval,
+    first_name VARCHAR2(255) NOT NULL,
+    last_name VARCHAR2(255) NOT NULL,
+    username VARCHAR2(15) NOT NULL,
+    email VARCHAR2(255) UNIQUE NOT NULL,
+    password VARCHAR2(255) NOT NULL,
+    is_account_non_expired NUMBER(1,0) DEFAULT 1 CHECK (is_account_non_expired IN (0,1)),
+    is_account_non_locked NUMBER(1,0) DEFAULT 1 CHECK (is_account_non_locked IN (0,1)),
+    is_credentials_non_expired NUMBER(1,0) DEFAULT 1 CHECK (is_credentials_non_expired IN (0,1)),
+    is_enabled NUMBER(1,0) DEFAULT 1 CHECK (is_enabled IN (0,1)),
+    profile_id NUMBER(19),
+    PRIMARY KEY (user_id),
+    CONSTRAINT FK_PROFILE_USERS FOREIGN KEY (profile_id) REFERENCES hub.profile(profile_id)
+);
+
 CREATE TABLE hub.course (
     course_id NUMBER(19) GENERATED AS IDENTITY,
     name VARCHAR2(255) NOT NULL UNIQUE,
@@ -7,8 +29,8 @@ CREATE TABLE hub.course (
 
 CREATE TABLE hub.topic (
     topic_id NUMBER(19) GENERATED AS IDENTITY,
-    title VARCHAR2(200) NOT NULL,
-    message VARCHAR2(255) NOT NULL,
+    title VARCHAR2(150) NOT NULL,
+    question VARCHAR2(255) NOT NULL,
     status VARCHAR2(10 CHAR) DEFAULT 'UNSOLVED' CHECK (status IN ('SOLVED', 'UNSOLVED')),
     created_at TIMESTAMP(6) WITH LOCAL TIME ZONE,
     user_id NUMBER(19),
