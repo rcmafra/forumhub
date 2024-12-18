3322e99c6ff665c74e2469e7a168ca6a18892584
RaulMafra <raulcesar.sm@gmail.com>
2024-12-18 15:03:22 -0300

    Remoção de método não utilizado

--- user/src/main/java/com/raul/forumhub/user/respository/UserRepository.java
+++ user/src/main/java/com/raul/forumhub/user/respository/UserRepository.java
@@ -4,11 +4,8 @@ import com.raul.forumhub.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    Optional<User> findByEmail(String email);
 }
 
