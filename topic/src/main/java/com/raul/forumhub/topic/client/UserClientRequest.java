230f3c85eec6ee97a0b7a2e2d705abbe1f4b6649
RaulMafra <raulcesar.sm@gmail.com>
2024-12-18 15:20:13 -0300

    Ajustes na classe de comunicação com o User client

--- topic/src/main/java/com/raul/forumhub/topic/client/UserClientRequest.java
+++ topic/src/main/java/com/raul/forumhub/topic/client/UserClientRequest.java
@@ -2,47 +2,62 @@ package com.raul.forumhub.topic.client;
 
 import com.raul.forumhub.topic.domain.Author;
 import com.raul.forumhub.topic.exception.RestClientException;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientRequestException;
 import org.springframework.web.reactive.function.client.WebClientResponseException;
 import reactor.core.publisher.Mono;
-import reactor.util.Logger;
-import reactor.util.Loggers;
 
+import java.nio.charset.StandardCharsets;
 import java.time.Duration;
+import java.util.Objects;
 import java.util.concurrent.TimeoutException;
 
 @Component
+@Validated
 public class UserClientRequest {
 
     private final WebClient webClient;
     private final UserClientProperties userClientProperties;
-    private final Logger logger = Loggers.getLogger(UserClientRequest.class);
 
     public UserClientRequest(WebClient webClient, UserClientProperties userClientProperties) {
         this.webClient = webClient;
         this.userClientProperties = userClientProperties;
     }
 
-    public Author getUserById(Long id) {
-        return this.webClient.get()
-                .uri(uriBuilder -> uriBuilder
-                        .scheme(userClientProperties.getSchema())
-                        .host(userClientProperties.getHost())
-                        .port(userClientProperties.getPort())
-                        .path("/api-forum/v1/forumhub/users/summary-info")
-                        .queryParam("user_id", id)
-                        .build())
-                .retrieve()
-                .bodyToMono(Author.class)
-                .timeout(Duration.ofMillis(1000))
-                .onErrorResume(TimeoutException.class, ex ->
-                        Mono.error(() -> new TimeoutException("Nenhuma resposta obtida dentro do tempo estimado")))
-                .onErrorResume(WebClientResponseException.class, ex ->
-                        Mono.error(new RestClientException(ex.getStatusCode(), ex.getResponseBodyAsString())))
-                .map(user -> new Author(user.getId(), user.getUsername(), user.getEmail(), user.getProfile()))
-                .log(logger)
-                .block();
+    public @Valid Author getUserById(Long id) {
+        try {
+            Mono<ResponseEntity<Author>> response = this.webClient.get()
+                    .uri(uriBuilder -> uriBuilder
+                            .scheme(userClientProperties.getSchema())
+                            .host(userClientProperties.getHost())
+                            .port(userClientProperties.getPort())
+                            .path("/api-forum/v1/forumhub/users/summary-info")
+                            .queryParam("user_id", id)
+                            .build())
+                    .accept(MediaType.APPLICATION_JSON)
+                    .acceptCharset(StandardCharsets.UTF_8)
+                    .retrieve()
+                    .toEntity(Author.class)
+                    .timeout(Duration.ofMillis(10000))
+                    .onErrorResume(WebClientResponseException.class, ex ->
+                            Mono.error(new RestClientException(ex.getStatusCode(), ex.getResponseBodyAsString())))
+                    .onErrorResume((ex) -> ex instanceof TimeoutException ^ ex instanceof WebClientRequestException ?
+                            Mono.error(() -> new RestClientException(HttpStatus.SERVICE_UNAVAILABLE, "Serviço de usuário indisponível")) :
+                            Mono.error(() -> new RestClientException(HttpStatus.SERVICE_UNAVAILABLE,
+                                    "Erro inesperado durante a comunicação com o serviço de usuário")));
+
+            return Objects.requireNonNull(response.block(),
+                    "Erro inesperado durante a comunicação com o serviço de usuário").getBody();
+
+        } catch (RestClientException ex) {
+            throw new RestClientException(ex.getHttpStatusCode(), ex.getMessage());
+        }
 
     }
 
