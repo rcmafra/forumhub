7feca4a8fbd4588ecddf5be39fd58f4404066d58
RaulMafra <raulcesar.sm@gmail.com>
2025-02-02 01:27:09 -0300

    Classe para captura das credenciais dos clients para persistência

---
+++ authorization-server/src/main/java/com/raul/forumhub/authorization/server/security/config/RegisteredClientProperties.java
@@ -0,0 +1,25 @@
+package com.raul.forumhub.authorization.server.security.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Setter
+@Getter
+@Configuration
+public class RegisteredClientProperties {
+
+    @Value("${registered.client.topic.name}")
+    String topicName;
+    @Value("${registered.client.topic.client-id}")
+    String topicClientID;
+    @Value("${registered.client.topic.password}")
+    String topicPassword;
+    @Value("${registered.client.user.name}")
+    String userName;
+    @Value("${registered.client.user.client-id}")
+    String userClientID;
+    @Value("${registered.client.user.password}")
+    String userPassword;
+}
