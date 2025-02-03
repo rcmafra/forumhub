af5ea8b2aa4e4a85ba7e80639d42563e5e796944
RaulMafra <raulcesar.sm@gmail.com>
2025-02-03 09:20:34 -0300

    Correção de exceções conhecidas

--- user/src/main/java/com/raul/forumhub/user/exception/handler/GlobalExceptionHandler.java
+++ user/src/main/java/com/raul/forumhub/user/exception/handler/GlobalExceptionHandler.java
@@ -45,17 +45,16 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(DataAccessException.class)
     private ResponseEntity<ExceptionEntity> dataAccessExceptionResolver(DataAccessException ex, HttpServletRequest request) {
-        ResponseEntity<ExceptionEntity> response = null;
         if (ex.getCause() instanceof org.hibernate.exception.ConstraintViolationException) {
-            response = new ResponseEntity<>(new ExceptionEntity(LocalDateTime.now(), HttpStatus.CONFLICT.value(),
+            return new ResponseEntity<>(new ExceptionEntity(LocalDateTime.now(), HttpStatus.CONFLICT.value(),
                     "Solicitação não processada", "Payload conflitante", request.getRequestURI()),
                     headers(), HttpStatus.CONFLICT);
         } else if ((ex.getCause() instanceof DataException && ((DataException) ex.getCause()).getErrorCode() == 22001)) {
-            response = new ResponseEntity<>(new ExceptionEntity(LocalDateTime.now(), HttpStatus.BAD_REQUEST.value(),
+            return new ResponseEntity<>(new ExceptionEntity(LocalDateTime.now(), HttpStatus.BAD_REQUEST.value(),
                     "Solicitação não processada", "Payload com valor muito grande", request.getRequestURI()),
                     headers(), HttpStatus.BAD_REQUEST);
         }
-        return response;
+        return this.notExpectedExceptionResolver(request);
     }
 
     @ExceptionHandler({HttpMessageNotReadableException.class, MethodArgumentTypeMismatchException.class})
