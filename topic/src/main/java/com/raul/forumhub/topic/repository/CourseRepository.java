9aaafd17182f8dafb621b8556aa91f9b01fdf270
RaulMafra <raulcesar.sm@gmail.com>
2025-03-07 23:39:36 -0300

    Removida a assinatura do método findCourseByName do CourseRepository

--- topic/src/main/java/com/raul/forumhub/topic/repository/CourseRepository.java
+++ topic/src/main/java/com/raul/forumhub/topic/repository/CourseRepository.java
@@ -4,11 +4,7 @@ import com.raul.forumhub.topic.domain.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface CourseRepository extends JpaRepository<Course, Long> {
-
-    Optional<Course> findCourseByName(String courseName);
 
 }
