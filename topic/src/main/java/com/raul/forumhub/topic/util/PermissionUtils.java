c81c51dfb0f982e8379a1a715c0b9123008667e4
RaulMafra <raulcesar.sm@gmail.com>
2025-03-15 14:33:59 -0300

    Substituído TopicServiceException por ValidationException para maior aderência quanto ao propósito do método validateTopicOwner

--- topic/src/main/java/com/raul/forumhub/topic/util/PermissionUtils.java
+++ topic/src/main/java/com/raul/forumhub/topic/util/PermissionUtils.java
@@ -2,7 +2,6 @@ package com.raul.forumhub.topic.util;
 
 import com.raul.forumhub.topic.domain.Author;
 import com.raul.forumhub.topic.domain.Profile;
-import com.raul.forumhub.topic.exception.TopicServiceException;
 import com.raul.forumhub.topic.exception.ValidationException;
 import lombok.experimental.UtilityClass;
 import lombok.extern.slf4j.Slf4j;
@@ -13,8 +12,8 @@ public class PermissionUtils {
 
     public void privilegeValidator(Long author_id, Author loggedUser) {
         if (!loggedUser.getId().equals(author_id) &&
-                !(loggedUser.getProfile().getProfileName().equals(Profile.ProfileName.MOD) ||
-                        loggedUser.getProfile().getProfileName().equals(Profile.ProfileName.ADM))) {
+            !(loggedUser.getProfile().getProfileName().equals(Profile.ProfileName.MOD) ||
+              loggedUser.getProfile().getProfileName().equals(Profile.ProfileName.ADM))) {
             throw new ValidationException("Privilégio insuficiente");
         }
         log.info("Privilege validator ==> Passou pela validação de privilégio");
@@ -22,7 +21,7 @@ public class PermissionUtils {
 
     public void validateTopicOwner(Long author_id, Long loggedUser) {
         if (!author_id.equals(loggedUser)) {
-            throw new TopicServiceException("O tópico fornecido não pertence ao autor atualmente logado");
+            throw new ValidationException("O tópico fornecido não pertence ao usuário atualmente logado");
         }
         log.info("Validate topic owner ==> Passou pela validação de autor proprietário do tópico");
     }
